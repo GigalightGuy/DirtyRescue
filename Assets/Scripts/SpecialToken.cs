d0d9c96f9e1543d5575cd81aac9ed8e23599ecaf
jinnFreddy <jorge.f.curto@gmail.com>
2024-01-18 16:28:07 +0000

    excluded enemy's layers

--- Assets/Scripts/SpecialToken.cs
+++ Assets/Scripts/SpecialToken.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpecialToken : MonoBehaviour
 {
+    [SerializeField] private Image _image;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             this.gameObject.SetActive(false);
+            _image.gameObject.SetActive(false);
             Scoring.Instance.AddScore(500);
         }
 
