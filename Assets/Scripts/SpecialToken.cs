2924f1f47f4db50f9cbf0445295f285f9eb1356b
jinnFreddy <jorge.f.curto@gmail.com>
2024-02-05 19:17:24 +0000

    QTE asset

--- Assets/Scripts/SpecialToken.cs
+++ Assets/Scripts/SpecialToken.cs
@@ -10,10 +10,18 @@ public class SpecialToken : MonoBehaviour
     [SerializeField] private Image _blankImage;
     [SerializeField] private Slider _quickTimeSlider;
     [SerializeField] private TMP_Text _keyToPress;
+
+    [SerializeField] private GameObject _animalAlive;
+    [SerializeField] private GameObject _animalDead;
+    public GameObject point;
+
     private float _timer;
     private bool _freeze;
     private bool _activated = false;
     private int _decreaseSpeed = 2;
+    private int _flyingSpeed = 10;
+    private bool _succeed = false;
+    private bool oneTimeeffects = true;
 
     KeyCode _key;
     KeyCode[] availableOptions = { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3 };
@@ -51,18 +59,33 @@ public class SpecialToken : MonoBehaviour
                 _quickTimeSlider.value += 1;
                 if (_quickTimeSlider.value == 10)
                 {
-                    _timer += Time.deltaTime;
-                    _keyToPress.text = "Saved!";
-                    if (_timer > 1f)
-                    {
-                        _keyToPress.gameObject.SetActive(false);
-                        this.gameObject.SetActive(false);
-                        _timer = 0;
-                    }
+                    _succeed = true;
+                }
+            }
+
+            if (_succeed)
+            {
+                if (oneTimeeffects)
+                {
                     _quickTimeSlider.gameObject.SetActive(false);
                     _freeze = true;
                     _blankImage.gameObject.SetActive(false);
                     Scoring.Instance.AddScore(500);
+                    oneTimeeffects = false;
+                }
+
+                _timer += Time.deltaTime;
+                _keyToPress.text = "Saved!";
+
+                _animalAlive.transform.right = _animalAlive.transform.position - point.transform.position;
+                _animalAlive.transform.position = Vector2.MoveTowards(_animalAlive.transform.position, point.transform.position, _flyingSpeed * Time.deltaTime);
+
+                if (_timer > 1f)
+                {
+                    _animalAlive.gameObject.SetActive(false);
+                    _keyToPress.gameObject.SetActive(false);
+                    this.gameObject.SetActive(false);
+                    _timer = 0;
                 }
             }
 
@@ -70,17 +93,21 @@ public class SpecialToken : MonoBehaviour
             {
                 _timer += Time.deltaTime;
                 _keyToPress.text = "Rip :(";
-                if (_timer > 1f)
+                _animalAlive.gameObject.SetActive(false);
+                _animalDead.gameObject.SetActive(true);
+                if (_timer > 1.0f)
                 {
                     _keyToPress.gameObject.SetActive(false);
                     this.gameObject.SetActive(false);
                     _timer = 0;
+                    _animalDead.gameObject.SetActive(false);
+                    _quickTimeSlider.gameObject.SetActive(false);
                 }
-                _quickTimeSlider.gameObject.SetActive(false);
                 _freeze = true;
             }
         }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
